6bad6eab42171401a2b3cf19ee7ebcc203dcf4f9
Caleb Martinez <accounts@calebmartinez.com>
2019-01-20 19:53:08 -0500

    Post stream commit and push.

--- src/platformer.js
+++ src/platformer.js
@@ -1,19 +1,132 @@
 var stop = false;
 var frameCount = 0;
-var fps, fpsInterval, startTime, now, then, elapsed, player, x, y;
+var fps, fpsInterval, startTime, now, then, elapsed, x, y;
 
-var player = {
-    x: 0,
-    y: 0,
-    height: null,
-    width: null,
-    el: null,
-    xAcc: 1,
-    yAcc: 1,
-    xVel: 0,
-    yVel: 0,
-    friction: .5,
-};
+const Player = class {
+    constructor({
+        x, xAcc, xMaxVel,
+        y, yAcc, yMaxVel,
+        width, height,
+        friction,
+        jumpAcc,
+    }) {
+
+        this.x = x;
+        this.xAcc = xAcc;
+        this.xVel = 0;
+        this.xMaxVel = xMaxVel;
+        this.y = y;
+        this.yAcc = yAcc;
+        this.yMaxVel = yMaxVel;
+        this.yVel = 0;
+        this.friction = friction;
+        this.jumpAcc = jumpAcc;
+
+        this.el = this.createDomElement(width, height);
+        this.draw = this.draw.bind(this);
+    }
+
+    update() {
+        if (controller[up] && this.grounded) {
+            this.yVel -= this.jumpAcc;
+        }
+
+        if (controller[down]) {
+            this.yVel += this.yAcc;
+
+        }
+
+        if (controller[left]) {
+            this.xVel -= this.xAcc;
+        }
+
+        if (controller[right]) {
+            this.xVel += this.xAcc;
+        }
+
+        if (this.xVel) {
+          this.xVel -= (this.xVel/Math.abs(this.xVel)) * this.friction;
+        }
+
+        // TERMINAL VELOCITY
+
+        this.yVel += gravity;
+        
+        this.terminalVelocity();
+
+        // X COLLISION
+        if(this.collision(this.xTrajectory, this.y)) {
+            while(!this.collision(
+                Math.floor(this.x + 1 * this.xVelSign),
+                this.y
+            )) { this.x += 1 * this.xVelSign; }
+            this.xVel = 0;
+        }
+
+        // Y COLLISION
+        if(this.collision(this.x, this.yTrajectory)) {
+            while(!this.collision(
+                this.x,
+                Math.floor(this.y + 1 * this.yVelSign)
+            )) { this.y += 1 * this.yVelSign; }
+            if (this.yVelSign === 1) { this.grounded = true; }
+            this.yVel = 0;
+        } else { this.grounded = false; }
+
+        this.x = Math.floor(this.x + this.xVel);
+        this.y = Math.floor(this.y + this.yVel);
+    }
+
+    draw() {
+        this.el.style.left = this.x + 'px';
+        this.el.style.top = this.y + 'px';
+    }
+
+    get xVelSign() { return this.xVel/Math.abs(this.xVel); }
+
+    get xTrajectory() { return Math.floor(this.x + this.xVel); }
+
+    get yVelSign() { return this.yVel/Math.abs(this.yVel); }
+
+    get yTrajectory() { return Math.floor(this.y + this.yVel); }
+
+    collision(x, y) { return detectCollision(x, y, this.width, this.height); }
+
+    terminalVelocity() {
+        if (Math.abs(this.xVel) > this.xMaxVel) {
+          this.xVel = this.xMaxVel * this.xVelSign;
+        }
+
+        if (Math.abs(this.yVel) > this.yMaxVel) {
+          this.yVel = this.yMaxVel * this.yVelSign;
+        }
+    }
+
+    createDomElement(width, height) {
+        const el = document.createElement('DIV');
+        el.style.width = width + 'px';
+        el.style.height = height + 'px';
+        el.style.position = 'absolute';
+        el.style.border = '1px solid black';
+        el.backgroundColor = '#831';
+        el.style.boxSizing = 'border-box';
+        el.style.top = this.y;
+        el.style.left = this.x;
+        window.document.body.appendChild(el);
+
+        return el;
+    }
+
+    get width() {
+        return this.el.offsetWidth;
+    }
+
+    get height() {
+        return this.el.offsetHeight;
+    }
+}
+
+var gravity = .5;
 
 var controller = {};
 
@@ -25,80 +138,51 @@ var right = 'ArrowRight';
 var down = 'ArrowDown';
 
 // initialize the timer variables and start the animation
-
 function init(fps) {
-    player.el = document.getElementById('player');
-    player.height = player.el.offsetHeight;
-    player.width = player.el.offsetWidth;
-    console.log(player);
-    console.log('Hello World');
+    const player = new Player({
+        x: 50,
+        y: 0,
+        height: 32,
+        width: 32,
+        el: null,
+        xAcc: 1,
+        yAcc: 1,
+        xVel: 0,
+        xMaxVel: 5,
+        yVel: 0,
+        yMaxVel: 18,
+        friction: .5,
+        grounded: false,
+        jumpAcc: 12,
+    });
+
+
     fpsInterval = 1000 / fps;
     then = Date.now();
     startTime = then;
     getCollisionMap();
-    console.log(collision);
-    update();
+    // console.log(collision);
+    update(player);
 }
 
 // the animation loop calculates time elapsed since the last loop
 // and only draws if your specified fps interval is achieved
 
-function update() {
-
-    // request another frame
-
-    requestAnimationFrame(update);
-
-    // calc elapsed time since last loop
-
+function update(player) {
+    requestAnimationFrame(() => update(player));
     now = Date.now();
     elapsed = now - then;
-
-    // if enough time has elapsed, draw the next frame
-
     if (elapsed > fpsInterval) {
-
-        // Get ready for next frame by setting then=now, but also adjust for your
-        // specified fpsInterval not being a multiple of RAF's interval (16.7ms)
         then = now - (elapsed % fpsInterval);
 
-        // Put your drawing code here
-        if (controller[up]) {
-            player.yVel -= player.yAcc;
-        }
+        player.update();
 
-        if (controller[down]) {
-            player.yVel += player.yAcc;
-        }
-
-        if (controller[left]) {
-            player.xVel -= player.xAcc;
-        }
-
-        if (controller[right]) {
-            player.xVel += player.xAcc;
-        }
-
-        if (player.xVel) {
-          player.xVel -= (player.xVel/Math.abs(player.xVel)) * player.friction;
-        }
-
-        if (player.yVel) {
-            player.yVel -= (player.yVel/Math.abs(player.yVel)) * player.friction;
-        }
-
-        detectCollision(); 
-        player.x += player.xVel;
-        player.y += player.yVel;
-        // console.log('Player position: ', player.x, ', ', player.y);
-        //console.log('Player velocity: ', player.xVel, ', ', player.yVel);
-        draw();
+        draw(player);
     }
 }
 
-function draw() {
-    player.el.style.left = player.x + 'px';
-    player.el.style.top = player.y + 'px';
+function draw(player) {
+    player.draw();
 }
 
 document.addEventListener('keydown', (event) => {
@@ -135,32 +219,33 @@ function getProperties(el) {
 }
 
 function getCollisionMap() {
-    console.log('Platforms: ', document.getElementsByClassName('platform'));
     var platforms = document.getElementsByClassName('platform');
 
     for (var key in platforms) {
-        // skip loop if the property is from prototype
         if (!platforms.hasOwnProperty(key)) continue;
 
         var platform = platforms[key];
 
+        platform.style.border = '1px solid black';
         var properties = getProperties(platform);
-        // console.log(properties);
         collision.push(properties);
     }
 
 }
 
-function detectCollision() {
+function detectCollision(x, y, width, height) {
+    collided = false;
     collision.forEach((platform) => {
         if (
-            player.x < platform.x + platform.width &&
-            player.x + player.width > platform.x &&
-            player.y < platform.y + platform.height &&
-            player.y + player.height > platform.y
+            x < platform.x + platform.width &&
+            x + width > platform.x &&
+            y < platform.y + platform.height &&
+            y + height > platform.y
         ) {
-            // collision detected!
-            console.log('Collided');
+            collided = true;
+            //console.log('collided', x, y, width, height);
+            return;
         }
     })
+    return collided;
 }
